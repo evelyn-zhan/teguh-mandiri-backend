8a552260eacf4c254c1196e9216a1cce0994c571
evelyn-zhan <evelynzanpx@gmail.com>
2026-05-01 16:11:11 +0700

    feat(supplier): add supplier model

---
+++ src/models/supplier.model.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+export interface ISupplier {
+    id: string
+    name: string
+    phone: string
+    email: string
+    address: string
+}
+
+const SupplierSchema = new Schema<ISupplier>(
+    {
+        id: {
+            type: Schema.Types.String,
+            required: true
+        },
+        name: {
+            type: Schema.Types.String,
+            required: true
+        },
+        phone: {
+            type: Schema.Types.String,
+            required: true
+        },
+        email: {
+            type: Schema.Types.String,
+            required: true
+        },
+        address: {
+            type: Schema.Types.String
+        }
+    },
+    {
+        timestamps: false
+    }
+)
+
+SupplierSchema.pre("save", async function (this: ISupplier) {
+    this.id = this.id.toUpperCase()
+})
+
+const SupplierModel = mongoose.model("supplier", SupplierSchema)
+
+export default SupplierModel
