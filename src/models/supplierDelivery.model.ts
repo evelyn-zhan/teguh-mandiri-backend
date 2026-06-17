143b6b6f692969603dd0cacba4fa3119c23855ad
evelyn-zhan <evelynzanpx@gmail.com>
2026-06-17 22:01:33 +0700

    feat: supplier delivery model

--- src/models/supplierDelivery.model.ts
+++ src/models/supplierDelivery.model.ts
@@ -8,8 +8,57 @@ export interface IDeliveredItem {
 }
 
 export interface ISupplierDelivery {
-    id: string
+    _id: string
     supplier: string
     items: IDeliveredItem[]
     deliveryDate: Date
 }
+
+const DeliveredItemSchema = new Schema<IDeliveredItem> (
+    {
+        name: {
+            type: Schema.Types.String,
+            required: true
+        },
+        quantity: {
+            type: Schema.Types.Number,
+            required: true
+        }
+    },
+    {
+        _id: false,
+        timestamps: false
+    }
+)
+
+const SupplierDeliverySchema = new Schema<ISupplierDelivery> (
+    {
+        _id: {
+            type: Schema.Types.String,
+            required: true
+        },
+        supplier: {
+            type: Schema.Types.String,
+            required: true
+        },
+        items: {
+            type: [DeliveredItemSchema],
+            required: true
+        },
+        deliveryDate: {
+            type: Schema.Types.Date,
+            required: true
+        }
+    },
+    {
+        timestamps: false
+    }
+)
+
+SupplierDeliverySchema.pre("save", async function (this: ISupplierDelivery) {
+    this._id = this._id.toUpperCase()
+})
+
+const SupplierDeliveryModel = mongoose.model("supplierDelivery", SupplierDeliverySchema)
+
+export default SupplierDeliveryModel
