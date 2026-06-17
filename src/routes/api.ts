a3d9401a986319dc5defc04f84170089817b0275
evelyn-zhan <evelynzanpx@gmail.com>
2026-06-17 10:05:29 +0700

    feat: purchase order routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import express from "express"
 
 import itemController from "../controllers/item.controller"
 import supplierController from "../controllers/supplier.controller"
+import purchaseOrderController from "../controllers/purchaseOrder.controller"
 
 const router = express.Router();
 
@@ -16,5 +17,11 @@ router.get("/suppliers/:id", supplierController.getSupplierById)
 router.post("/suppliers", supplierController.addSupplier)
 router.put("/suppliers/:id", supplierController.updateSupplier)
 router.delete("/suppliers/:id", supplierController.deleteSupplier)
+
+router.get("/procurement/purchase-orders", purchaseOrderController.getAllOrders)
+router.get("/procurement/purchase-orders/:id", purchaseOrderController.getOrderById)
+router.post("/procurement/purchase-orders", purchaseOrderController.addOrder)
+router.put("/procurement/purchase-orders/:id", purchaseOrderController.updateOrder)
+router.delete("/procurement/purchase-orders/:id", purchaseOrderController.deleteOrder)
 
 export default router
