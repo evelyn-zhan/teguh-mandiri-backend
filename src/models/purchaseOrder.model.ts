e82a14f4748c4a892423de3f89662b807832270b
evelyn-zhan <evelynzanpx@gmail.com>
2026-06-17 10:03:24 +0700

    feat: purchase order model

---
+++ src/models/purchaseOrder.model.ts
@@ -0,0 +1,69 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+export interface IOrderItem {
+    name: string
+    quantity: number
+    received: number
+}
+
+export interface IPurchaseOrder {
+    id: string
+    supplier: string
+    items: IOrderItem[]
+    expectedDeliveryDate: Date
+}
+
+const OrderItemSchema = new Schema<IOrderItem> (
+    {
+        name: {
+            type: Schema.Types.String,
+            required: true
+        },
+        quantity: {
+            type: Schema.Types.Number,
+            required: true
+        },
+        received: {
+            type: Schema.Types.Number,
+            default: 0
+        }
+    },
+    {
+        _id: false,
+        timestamps: false
+    }
+)
+
+const PurchaseOrderSchema = new Schema<IPurchaseOrder> (
+    {
+        id: {
+            type: Schema.Types.String,
+            required: true
+        },
+        supplier: {
+            type: Schema.Types.String,
+            required: true
+        },
+        items: {
+            type: [OrderItemSchema],
+            required: true
+        },
+        expectedDeliveryDate: {
+            type: Schema.Types.Date,
+            required: true
+        }
+    },
+    {
+        _id: false
+    }
+)
+
+PurchaseOrderSchema.pre("save", async function (this: IPurchaseOrder) {
+    this.id = this.id.toUpperCase()
+})
+
+const PurchaseOrderModel = mongoose.model("purchaseOrder", PurchaseOrderSchema)
+
+export default PurchaseOrderModel
